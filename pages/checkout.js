372b3937d686253fdb6f39f916d53de5eba56132
Angmo <55769383+Angmo@users.noreply.github.com>
2022-08-06 12:50:06 +0200

    extra

--- pages/checkout.js
+++ pages/checkout.js
@@ -9,63 +9,61 @@ import Liste from "../components/Liste";
 import Header from "../components/Header";
 
 const Checkout = () => {
-  const router = useRouter();
-  const axios = require("axios").default;
-  const [productList, setProductList] = useState(null);
+    const router = useRouter();
+    const axios = require("axios").default;
+    const [productList, setProductList] = useState(null);
 
-  useEffect(() => {
-    axios
-      .get("/api/hello")
-      .then(function (response) {
-        // handle success
-        setProductList(response.data.products);
-        console.log("asd", productList);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
-  }, []);
+    useEffect(() => {
+        axios
+            .get("/api/hello")
+            .then(function (response) {
+                // handle success
+                setProductList(response.data.products);
+                console.log("asd", productList);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    }, []);
 
-  return (
-    <div>
-      <Header></Header>
-      <div>
-        <div className="checkout-container">
-          Checkout page
-          <div>
-            <ShoppingCartIcon />
-            <Button type="button" onClick={() => router.push("/payment")}>
-              Einkaufswagen
-            </Button>
-            {/* <Button href="localhost:3000/payment">Einkaufswagen</Button> */}
-          </div>
-        </div>
-
+    return (
         <div>
-            <div className='checkout-container'>
-            Checkout page
+            <Header></Header>
+            <div>
+                <div className="checkout-container">
+                    Checkout page
+                    <div>
+                        <ShoppingCartIcon />
+
+                        <Button
+                            type="button"
+                            onClick={() => router.push("/payment")}
+                        >
+                            Einkaufswagen
+                        </Button>
+                    </div>
+                </div>
                 <div>
-                    <ShoppingCartIcon/>
-                    
-                    <Button type="button" onClick={() => router.push('/payment')}>
-                        Einkaufswagen
-                    </Button>
+                    {!!productList &&
+                        productList.map((item) => {
+                            return (
+                                <Card
+                                    key={item.id}
+                                    imgsrc={item.srcImage}
+                                    title={item.title}
+                                    description={"Sehr gut"}
+                                    price={item.price}
+                                    id={item.id}
+                                ></Card>
+                            );
+                        })}
                 </div>
             </div>
-            <div>
-                {!!productList && productList.map((item) => {
-                    return(
-                        <Card key={item.id} imgsrc={item.srcImage} title={item.title} description={'Sehr gut'} price={item.price} id={item.id}></Card>
-                    )
-                })}
-            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 export default Checkout;
--- pages/product/[pid].js
+++ pages/product/[pid].js
@@ -1,20 +1,40 @@
-import { Box } from '@mui/material'
-import { useRouter } from 'next/router'
-import Image from 'next/image'
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { Box } from "@mui/material";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const pid = ({productList}) => {
-
-    const router = useRouter()
-    const { pid } = router.query
+const pid = ({ productList }) => {
+    const router = useRouter();
+    const { pid } = router.query;
 
     return (
         // Sticky Image
-        <Box>
-            <Box sx={{position: 'sticky'}}>
-                <Image src='/images/tv.jpg' width={'1000px'} height={'1200px'}></Image>
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: { xs: "column", md: "row" },
+                width: 2000,
+                height: 2000,
+            }}
+        >
+            <Box
+                sx={{
+                    position: "sticky",
+                    margin: 0,
+                    left: 0,
+                    top: 0,
+                    width: 500,
+                    height: 500,
+                    zindex: 10,
+                }}
+            >
+                <Image
+                    src="/images/tv.jpg"
+                    width={"10000px"}
+                    height={"10000px"}
+                ></Image>
             </Box>
 
             <Box>
@@ -27,9 +47,7 @@ const pid = ({productList}) => {
                 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
             </Box>
         </Box>
+    );
+};
 
-        
-    )
-}
-
-export default pid
+export default pid;
